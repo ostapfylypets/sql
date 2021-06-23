bd4b7550e3e93669702b41d26272c1b98d144cbd
ostapfylypets <ostapfull21@gmail.com>
2021-06-23 06:43:26 +0300

    dz1

--- node/dz1/dz1.js
+++ node/dz1/dz1.js
@@ -1,5 +1,5 @@
-const link1800 =`${__dirname}/users/1800`
-const link2000 =`${__dirname}/users/2000`
+const link1800 = `${__dirname}/users/1800`
+const link2000 = `${__dirname}/users/2000`
 
 const fs = require('fs');
 const path = require("path");
@@ -9,11 +9,11 @@ fs.readdir(link1800, (err, data) => {
         console.log('__________________________________');
         console.log(err);
         console.log('__________________________________');
-        return
+        return;
     }
     data.forEach(value => {
-        fs.rename(path.join(link1800,value),path.join(link2000,value),(err)=>{
-            if(err){
+        fs.rename(path.join(link1800, value), path.join(link2000, value), (err) => {
+            if (err) {
                 console.log('__________________________________');
                 console.log(err);
                 console.log('__________________________________');
@@ -28,10 +28,10 @@ fs.readdir(link2000, (err, data) => {
         console.log('__________________________________');
         console.log(err);
         console.log('__________________________________');
-        return
+        return;
     }
-   data.forEach(value => {
-        fs.rename(path.join(link2000,value), path.join(link1800,value), (err) => {
+    data.forEach(value => {
+        fs.rename(path.join(link2000, value), path.join(link1800, value), (err) => {
             if (err) {
                 console.log('__________________________________');
                 console.log(err);
@@ -42,90 +42,82 @@ fs.readdir(link2000, (err, data) => {
 })
 
 
-
-
 // -------------------------------------------------------------------------------
 
 
+const fs = require('fs');
 
 
+const path = require("path");
+const girls = `${__dirname}/girls`
+const boys = `${__dirname}/boys`;
+
+let gender = path.join(__dirname, "users");
 
 
+fs.mkdir(girls, {recursive: true}, (err) => {
+    if (err) {
+        console.log(err);
+    }
+    fs.mkdir(boys, {recursive: true}, (err) => {
+        if (err) {
+            console.log(err);
+        }
 
-// const fs = require('fs');
-//
-//
-// const path = require("path");
-// const girls = `${__dirname}/girls`
-// const boys = `${__dirname}/boys`
-//
-// let gender=path.join(__dirname, "users")
-//
-//
-//     fs.mkdir(girls, {recursive: true}, (err) => {
-//         if (err) {
-//             console.log(err);
-//         }
-//         fs.mkdir(boys, {recursive: true}, (err) => {
-//             if (err) {
-//                 console.log(err);
-//             }
-//
-//
-//
-//             let array;
-//             let jsonchik;
-//
-//
-//             fs.readdir(gender, (err, dir) => {
-//                 if (err) {
-//                     console.log('__________________________________');
-//                     console.log(err);
-//                     console.log('__________________________________');
-//                     return
-//                 }
-//                 dir.forEach(Direct => {
-//                     fs.readdir(path.join(gender, Direct), ((err, data) => {
-//                         if (err) {
-//                             console.log('__________________________________');
-//                             console.log(err);
-//                             console.log('__________________________________');
-//                             return
-//                         }
-//                         data.forEach(dataDaughter => {
-//                                 fs.readFile(path.join(gender, Direct, dataDaughter), ((err2, data) => {
-//                                     console.log(path.join(gender, Direct, dataDaughter))
-//                                     array = data.toString();
-//                                     jsonchik = JSON.parse(array)
-//                                     if (jsonchik.gender === 'male') {
-//                                         fs.rename(path.join(gender, Direct, dataDaughter), path.join(boys, dataDaughter), (err) => {
-//                                             if (err) {
-//                                                 console.log('__________________________________');
-//                                                 console.log(err);
-//                                                 console.log('__________________________________');
-//                                             }
-//
-//                                         })
-//                                     } else if (jsonchik.gender === 'female') {
-//                                         fs.rename(path.join(gender, Direct, dataDaughter), path.join(girls, dataDaughter), (err) => {
-//                                             if (err) {
-//                                                 console.log('__________________________________');
-//                                                 console.log(err);
-//                                                 console.log('__________________________________');
-//                                             }
-//
-//                                         })
-//                                     }
-//                                 }))
-//                             }
-//                         )
-//                     }))
-//                 })
-//
-//             })
-//
-//         })
-//     })
+
+        let array;
+        let jsonchik;
+
+
+        fs.readdir(gender, (err, dir) => {
+            if (err) {
+                console.log('__________________________________');
+                console.log(err);
+                console.log('__________________________________');
+                return;
+            }
+            dir.forEach(Direct => {
+                fs.readdir(path.join(gender, Direct), ((err, data) => {
+                    if (err) {
+                        console.log('__________________________________');
+                        console.log(err);
+                        console.log('__________________________________');
+                        return;
+                    }
+                    data.forEach(dataDaughter => {
+                            fs.readFile(path.join(gender, Direct, dataDaughter), ((err2, data) => {
+                                console.log(path.join(gender, Direct, dataDaughter))
+                                array = data.toString();
+                                jsonchik = JSON.parse(array);
+                                if (jsonchik.gender === 'male') {
+                                    fs.rename(path.join(gender, Direct, dataDaughter), path.join(boys, dataDaughter), (err) => {
+                                        if (err) {
+                                            console.log('__________________________________');
+                                            console.log(err);
+                                            console.log('__________________________________');
+                                        }
+
+                                    })
+                                } else if (jsonchik.gender === 'female') {
+                                    fs.rename(path.join(gender, Direct, dataDaughter), path.join(girls, dataDaughter), (err) => {
+                                        if (err) {
+                                            console.log('__________________________________');
+                                            console.log(err);
+                                            console.log('__________________________________');
+                                        }
+
+                                    })
+                                }
+                            }))
+                        }
+                    )
+                }))
+            })
+
+        })
+
+    })
+})
 
 
 
